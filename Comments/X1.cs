546336e7e48ddc07c5e77cf1027aab6bda29894d
Dom Newman <dom.newman@mindlinksoft.com>
2026-09-16 10:44:55 +0100

    It works!

--- Comments/X1.cs
+++ Comments/X1.cs
@@ -2,12 +2,11 @@
 
 public class X1
 {
-    public static int SumOfSquares(int q, int z)
+    public static int SumOfSquares(int lowerBound, int upperBound)
     {
         int p = 0;
 
-        // Iterate from lower bound (q) to upper bound (z)
-        for (int i = q; i <= z; i++)
+        for (int i = lowerBound; i <= upperBound; i++)
         {
             // Add square of each number in the range
             p += N(i);
